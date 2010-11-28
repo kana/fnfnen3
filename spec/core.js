cbb8ca96bca43ce697b6da3bd5d4bad392f3364c
kana <nicht@s8.xrea.com>
2010-11-29 01:09:27 +0900

    Fix foldexpr for spec file

--- spec/core.js
+++ spec/core.js
@@ -258,4 +258,4 @@ describe('Core', function () {
 // __END__
 // vim: expandtab shiftwidth=2 softtabstop=2
 // vim: foldmethod=expr
-// vim: foldexpr=getline(v\:lnum)=~#'\\v(<describe>|<it>).*<function>\\s*\\([^()]*\\)\\s*\\{'?'a1'\:(getline(v\:lnum)=~#'^\\s*});'?'s1'\:'=')
+// vim: foldexpr=getline(v\:lnum)=~#'\\v<x?(describe|it)>.*<function>\\s*\\([^()]*\\)\\s*\\{'?'a1'\:(getline(v\:lnum)=~#'^\\s*});'?'s1'\:'=')
