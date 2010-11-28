1a6cdb5de1e79d775ef040413b8371d654b52170
kana <nicht@s8.xrea.com>
2010-11-28 12:35:27 +0900

    Implement prafbe.tokenize

--- prafbe.js
+++ prafbe.js
@@ -3,6 +3,37 @@ var prafbe = {};
 
 
 
+prafbe.list_bigrams = function (s)
+{
+  // FIXME: NIY
+  return [s];
+};
+
+
+
+
+prafbe.tokenize = function (s)
+{
+  return (
+    s
+    .replace(/([\u0000-\u007F]+)|([^\u0000-\u007F]+)/g, function (_, p1, p2) {
+      if (p1) {
+        return p1.replace(/[^A-Za-z0-9.\-$!']+/g, ' ')
+      } else {
+        return ' ' + prafbe.list_bigrams(p2).join(' ') + ' ';
+      }
+    })
+    .split(/\s+/)
+    .map(function (x) {return x.replace(/^\.+/, '');})
+    .map(function (x) {return x.replace(/\.+$/, '');})
+    .filter(function (x) {return x != '';})
+    .filter(function (x) {return !(/^\d+$/.test(x));})
+  );
+};
+
+
+
+
 // __END__
 // vim: expandtab shiftwidth=2 softtabstop=2
 // vim: foldmethod=marker
--- spec/core.js
+++ spec/core.js
@@ -1,5 +1,35 @@
 describe('Core', function () {
-  // FIXME: NIY
+  describe('tokenize', function () {
+    var _ = function (s, tokens) {
+      expect(prafbe.tokenize(s)).toEqual(tokens);
+    };
+
+    it('should split plain English words', function () {
+      _('foo bar baz', ['foo', 'bar', 'baz']);
+      _('  a  b  c  ', ['a', 'b', 'c']);
+    });
+    it('should split plain English words in a sentence', function () {
+      _('It works.', ['It', 'works']);
+    });
+    it('should count some charcters as ordinary alphabets', function () {
+      _('send more $', ['send', 'more', '$']);
+      _('a-b-c d-e-f', ['a-b-c', 'd-e-f']);
+      _('wow! really?', ['wow!', 'really']);
+      _('you\'ll know', ['you\'ll', 'know']);
+    });
+    it('should ignore digit-only words', function () {
+      _('Sum of 12 and 34 is 46.', ['Sum', 'of', 'and', 'is']);
+      _('2dx gold', ['2dx', 'gold']);
+    });
+    it('should count domain-like part as a word', function () {
+      _('127.0.0.1 = localhost', ['127.0.0.1', 'localhost']);
+      _('did.you.know', ['did.you.know']);
+    });
+    it('should tokenize multibyte characters by 2-gram', function () {
+      _('あいうえお', ['あい', 'いう', 'うえ', 'えお']);
+      _('foo あいうえお bar', ['foo', 'あい', 'いう', 'うえ', 'えお', 'bar']);
+    });
+  });
 });
 
 
