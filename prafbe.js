943958612ef7467d32eebbabf0818f3f9041c5db
kana <nicht@s8.xrea.com>
2010-11-28 12:10:22 +0900

    Create initial structure

---
+++ prafbe.js
@@ -0,0 +1,8 @@
+var prafbe = {};
+
+
+
+
+// __END__
+// vim: expandtab shiftwidth=2 softtabstop=2
+// vim: foldmethod=marker
---
+++ spec/core.js
@@ -0,0 +1,11 @@
+describe('Core', function () {
+  // FIXME: NIY
+});
+
+
+
+
+// __END__
+// vim: expandtab shiftwidth=2 softtabstop=2
+// vim: foldmethod=expr
+// vim: foldexpr=getline(v\:lnum)=~#'\\v(<describe>|<it>).*<function>\\s*\\([^()]*\\)\\s*\\{'?'a1'\:(getline(v\:lnum)=~#'^\\s*});'?'s1'\:'=')
